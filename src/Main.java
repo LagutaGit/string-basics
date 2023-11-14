2fb8bbee2098c21eb519087da88c46cc7dc07744
Laguta Roman <roma_laguta@mail.ru>
2023-11-15 00:30:15 +0900

    Второй ролик дядьки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
 public class Main {
     public static void main(String[] args) {
+        dyadka1();
+//        konspekt1();
 
+    }
+
+    public static void dyadka1() {
+        String phone = "896-053-78-87";
+
+        if (phone.length() == 10) {
+            phone = '7' + phone;
+        } else if (phone.length() > 11) {
+//            страшное предупреждение через throw
+            throw new RuntimeException("Телефон слишком длинный");
+        } else if (phone.length() < 10) {
+            throw new RuntimeException("Телефон слишком короткий");
+        } else if (phone.length() == 11 && phone.charAt(0) != '7') {
+            throw new RuntimeException("Среди нас посторонний");
+        }
+
+        System.out.println("phone = " + phone);
+
+    }
+
+    public static void konspekt1() {
+        String address = "Адрес, который мы хотим указать при доставке";
+        String street = "Манежная";
+        street = "Манежная площадь";
+        System.out.println(street);
+
+        String streetSecond = "Манежная";
+        streetSecond = streetSecond + " площадь";
+        System.out.println("Адресс магазина - " + streetSecond);
+
+        String numberPhone = "9654132154";
+        numberPhone = '7' + numberPhone;
+        System.out.println("Номер телефона - " + numberPhone);
+
+        String phone = "965413";
+        if (phone.length() == 10) {
+            phone = '7' + phone;
+        } else if (phone.length() > 11) {
+            throw new RuntimeException("В телефоне больше 11 цифр");
+        } else if (phone.length() < 7) {
+            throw new RuntimeException("В телефоне меньше 7 цифр");
+        }
+        System.out.println("phone = " + phone);
     }
 }
