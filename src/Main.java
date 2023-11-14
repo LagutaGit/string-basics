a8352b5da0dd6ef3b7d04df6ea6e3f008af009f8
Laguta Roman <roma_laguta@mail.ru>
2023-11-15 00:39:39 +0900

    Второй конспект

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        dyadka1();
-//        konspekt1();
+//        dyadka1();
+        konspekt1();
 
     }
 
     public static void dyadka1() {
-        String phone = "896-053-78-87";
-
+        String phone = "+8960537887";
+//        Метод замены символов
+        phone = phone.replace("-", "");
+        phone = phone.replace(" ", "");
+        phone = phone.replace("+", "");
         if (phone.length() == 10) {
             phone = '7' + phone;
         } else if (phone.length() > 11) {
@@ -24,27 +27,31 @@ public class Main {
     }
 
     public static void konspekt1() {
-        String address = "Адрес, который мы хотим указать при доставке";
-        String street = "Манежная";
-        street = "Манежная площадь";
-        System.out.println(street);
+//        String address = "Адрес, который мы хотим указать при доставке";
+//        String street = "Манежная";
+//        street = "Манежная площадь";
+//        System.out.println(street);
+//
+//        String streetSecond = "Манежная";
+//        streetSecond = streetSecond + " площадь";
+//        System.out.println("Адресс магазина - " + streetSecond);
+//
+//        String numberPhone = "9654132154";
+//        numberPhone = '7' + numberPhone;
+//        System.out.println("Номер телефона - " + numberPhone);
+//
+//        String phone = "965413";
+//        if (phone.length() == 10) {
+//            phone = '7' + phone;
+//        } else if (phone.length() > 11) {
+//            throw new RuntimeException("В телефоне больше 11 цифр");
+//        } else if (phone.length() < 7) {
+//            throw new RuntimeException("В телефоне меньше 7 цифр");
+//        }
+//        System.out.println("phone = " + phone);
 
-        String streetSecond = "Манежная";
-        streetSecond = streetSecond + " площадь";
-        System.out.println("Адресс магазина - " + streetSecond);
-
-        String numberPhone = "9654132154";
-        numberPhone = '7' + numberPhone;
-        System.out.println("Номер телефона - " + numberPhone);
-
-        String phone = "965413";
-        if (phone.length() == 10) {
-            phone = '7' + phone;
-        } else if (phone.length() > 11) {
-            throw new RuntimeException("В телефоне больше 11 цифр");
-        } else if (phone.length() < 7) {
-            throw new RuntimeException("В телефоне меньше 7 цифр");
-        }
-        System.out.println("phone = " + phone);
+        String street2 = "манежная";
+        String streetTrue = street2.replace("м","М");
+        System.out.println(streetTrue);
     }
 }
