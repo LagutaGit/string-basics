37c9e7638402cc53b43beb7729712fe30655b749
Laguta Roman <roma_laguta@mail.ru>
2023-11-16 23:25:43 +0900

    ДЗ Строки готово

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 //        dyadka1();
-        konspekt1();
-
+//        konspekt1();
+//        homeWork1();
+//        homeWork2();
+        homeWork3();
     }
 
     public static void dyadka1() {
-        String phone = "+8960537887";
+        String phone = "9605378877";
 //        Метод замены символов
         phone = phone.replace("-", "");
         phone = phone.replace(" ", "");
@@ -23,7 +25,12 @@ public class Main {
         }
 
         System.out.println("phone = " + phone);
-
+        String expectedPhone = "79605378877";
+        if (phone.equals(expectedPhone)) {
+            System.out.println("Успех");
+        } else {
+            System.out.println("Неудача");
+        }
     }
 
     public static void konspekt1() {
@@ -50,8 +57,29 @@ public class Main {
 //        }
 //        System.out.println("phone = " + phone);
 
-        String street2 = "манежная";
-        String streetTrue = street2.replace("м","М");
-        System.out.println(streetTrue);
+//        String street2 = "манежная";
+//        String streetTrue = street2.replace("м","М");
+//        System.out.println(streetTrue);
+    }
+
+    public static void homeWork1() {
+//        Zadacha 1
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("ФИО Сотрудника - " + fullName);
+    }
+    public static void homeWork2() {
+        String fullName = "koval liam hemtsword";
+
+        String toUpperCaseName = fullName.toUpperCase();
+        System.out.println(toUpperCaseName);
+    }
+    public static void homeWork3() {
+        String fullName = "Королёв Иван Романович";
+
+        String toUpperCaseName = fullName.replace("ё", "е");
+        System.out.println(toUpperCaseName);
     }
 }
